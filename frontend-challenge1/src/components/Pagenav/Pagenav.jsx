7701d57ef2b57a2890a26f822c4159800bfcd528
William La <william.la59@gmail.com>
2022-04-01 15:08:34 -0400

    altered package.json

--- frontend-challenge1/src/components/Pagenav/Pagenav.jsx
+++ frontend-challenge1/src/components/Pagenav/Pagenav.jsx
@@ -14,9 +14,9 @@ export const Pagenav = () => {
             Explore
         </div>
         <div className='nav-container'>
-            <Button className="pagenav-btn" style={currentRoute === '/' ? {color: "#27ae60", backgroundColor: "#f5f5f5"} : {color: "##8b8b8b", backgroundColor: "#fff"}}><Link to='/'>Challenges</Link></Button>
-            <Button className="pagenav-btn" style={currentRoute === '/labs' ? {color: "#27ae60", backgroundColor: "#f5f5f5"} : {color: "##8b8b8b", backgroundColor: "#fff"}}><Link to='/labs'>Labs</Link></Button>
-            <Button className="pagenav-btn" style={currentRoute === '/projects' ? {color: "#27ae60", backgroundColor: "#f5f5f5"} : {color: "##8b8b8b", backgroundColor: "#fff"}}><Link to='/projects'>Projects</Link></Button>
+            <Button className="pagenav-btn" style={currentRoute === '/' ? {color: "#27ae60", backgroundColor: "#f5f5f5"} : {color: "##8b8b8b", backgroundColor: "#fff"}} onClick={setCurrentRoute("/")}><Link to='/'>Challenges</Link></Button>
+            <Button className="pagenav-btn" style={currentRoute === '/labs' ? {color: "#27ae60", backgroundColor: "#f5f5f5"} : {color: "##8b8b8b", backgroundColor: "#fff"}} onClick={setCurrentRoute("/labs")}><Link to='/labs'>Labs</Link></Button>
+            <Button className="pagenav-btn" style={currentRoute === '/projects' ? {color: "#27ae60", backgroundColor: "#f5f5f5"} : {color: "##8b8b8b", backgroundColor: "#fff"}} onClick={setCurrentRoute("/projects")}><Link to='/projects'>Projects</Link></Button>
         </div>
     </div>
   )
